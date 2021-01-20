aafc5db7cf9a1b699f37d97f63065192d5249606
Sunil Kumar <sunil.kumar@nescode.com>
2021-01-20 11:41:33 +0530

    Allowed host updated

--- hd122020/settings.py
+++ hd122020/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'scxc-nlikkc@4oves70-xw5x)nlg1=h1q(x@s20l1_123&9vgi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
